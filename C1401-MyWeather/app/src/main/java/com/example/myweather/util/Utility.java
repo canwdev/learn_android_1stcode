e824ad8daf4b692577a4d15ea95b16d58fda4a7b
canw <c2097@live.com>
2017-09-26 10:29:26 +0800

    traverse city list function complete!

--- C1401-MyWeather/app/src/main/java/com/example/myweather/util/Utility.java
+++ C1401-MyWeather/app/src/main/java/com/example/myweather/util/Utility.java
@@ -9,19 +9,23 @@ import com.example.myweather.db.Province;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.litepal.crud.DataSupport;
 
 public class Utility {
 
-    // 解析JSON的省级数据
+    // 解析JSON的省级数据，保存到数据库
     public static boolean handleProvinceResponse(String response) {
-        if (TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {
             try {
+                DataSupport.deleteAll(Province.class);
+
                 JSONArray allProvinces = new JSONArray(response);
                 for (int i = 0; i < allProvinces.length(); i++) {
                     JSONObject provinceObject = allProvinces.getJSONObject(i);
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
                 }
                 return true;
             } catch (JSONException e) {
@@ -35,6 +39,7 @@ public class Utility {
     public static boolean handleCityResponse(String response, int provinceId) {
         if (!TextUtils.isEmpty(response)) {
             try {
+                DataSupport.deleteAll(City.class);
                 JSONArray allCities = new JSONArray(response);
                 for (int i = 0; i < allCities.length(); i++) {
                     JSONObject cityObjcet = allCities.getJSONObject(i);
@@ -54,14 +59,16 @@ public class Utility {
 
     // 解析JSON县级数据
     public static boolean handleCountyResponse(String response, int cityId) {
-        if (TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {
             try {
+                DataSupport.deleteAll(County.class);
                 JSONArray allCounties = new JSONArray(response);
                 for (int i = 0; i < allCounties.length(); i++) {
                     JSONObject countyObject = allCounties.getJSONObject(i);
                     County county = new County();
                     county.setCountyName(countyObject.getString("name"));
                     county.setCityId(countyObject.getInt("id"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
                     county.setCityId(cityId);
                     county.save();
                 }
